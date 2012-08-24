b6d2996f3382ae0aef32d53b11d9741cd3335033
Pavel Kiselev <pk.kiselev@gmail.com>
2012-08-24 16:20:52 +0300

    SAAS files update

--- resources/scss/config.rb
+++ resources/scss/config.rb
@@ -2,7 +2,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "../css"
 sass_dir = "sass"
 images_dir = "../images"
 javascripts_dir = "../js"
@@ -10,7 +10,7 @@ javascripts_dir = "../js"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
-output_style = :expanded
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
